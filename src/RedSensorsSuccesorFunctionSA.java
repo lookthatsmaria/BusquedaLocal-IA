afc64040d07dfa4b2d2469d1b18b4cd21c8753c3
LookThatsMari <mariama5116@gmail.com>
2021-03-22 21:33:26 +0100

    bugs fixed (or not, idk)

--- src/RedSensorsSuccesorFunctionSA.java
+++ src/RedSensorsSuccesorFunctionSA.java
@@ -5,32 +5,36 @@ import aima.search.framework.SuccessorFunction;
 
 public class RedSensorsSuccesorFunctionSA implements SuccessorFunction{
 
-    public List getSuccessors(Object aState){
-        ArrayList retVal= new ArrayList();
-        RedSensorsState state=(RedSensorsState) aState;
-				int min = state.getNcent();
-				int max = state.getnElements();
-				RedSensorsState newState;
-        double [][] adjacencyMatrix = state.getAdjacencyMatrix();
-				int node = (int)(Math.random()*(max-min+1)+min);
-				int newConnection = (int)(Math.random()*(max-min+1)+min);
-				int oldConnection = (int) adjacencyMatrix[node][node];
-				int limit = newConnection < min? 25:3;
-				while(!state.canConnect(newConnection,limit) || !state.findLoop(node,newConnection)){
-					node = (int)(Math.random()*(max-min+1)+min);
-					newConnection = (int)(Math.random()*(max-min+1)+min);
-          oldConnection = (int) adjacencyMatrix[node][node];
-				}
-				newState = new RedSensorsState(
-						state.getNcent(),
-						state.getnElements() - state.getNcent(),
-						state.getDist(),
-						adjacencyMatrix,
-						state.getDatacenters(),
-						state.getSensors());
+    public List getSuccessors(Object aState) {
+        ArrayList retVal = new ArrayList();
+        RedSensorsState state = (RedSensorsState) aState;
+        int min = state.getNcent();
+        int max = state.getnElements();
+        RedSensorsState newState;
+        double[][] adjacencyMatrix = state.getAdjacencyMatrix();
+        Random rand = new Random();
+        int range = max - min + 1;
+        int node = rand.nextInt((range) + min);
+        int newConnection = rand.nextInt((range) + min);
+        int oldConnection = (int) adjacencyMatrix[node][node];
+        int limit = newConnection < min ? 25 : 3;
+        while (newConnection != node && newConnection != oldConnection &&
+                (!state.findLoop(node, newConnection) || !state.canConnect(newConnection, limit))) {
+            node = rand.nextInt((range) + min);
+            newConnection = rand.nextInt((range) + min);
+            oldConnection = (int) adjacencyMatrix[node][node];
+        }
+        newState = new RedSensorsState(
+                state.getNcent(),
+                state.getnElements() - state.getNcent(),
+                state.getDist(),
+                adjacencyMatrix,
+                state.getDatacenters(),
+                state.getSensors());
         double throughput = adjacencyMatrix[node][oldConnection];
-				newState.newConnection(node,oldConnection,newConnection,throughput);
-				retVal.add(new Successor("NEW CONNECTION", newState));
+        newState.newConnection(node, oldConnection, newConnection, throughput);
+        retVal.add(new Successor("NEW CONNECTION", newState));
         return retVal;
     }
+
 }
